d6439386fbdacc0cd249b805307a7ade458054b8
JLoewenguth <jrm.lguth@gmail.com>
2022-12-07 13:43:32 +0100

    etudiant BDD

--- src/Form/EtudiantType.php
+++ src/Form/EtudiantType.php
@@ -10,7 +10,6 @@ use Symfony\Component\Validator\Constraints\Date;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Validator\Constraints\Choice;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class EtudiantType extends AbstractType
@@ -22,8 +21,8 @@ class EtudiantType extends AbstractType
             ->add('prenom_etudiant', TextType::class)
             ->add('email_etudiant', TextType::class)
             ->add('phone_etudiant', TextType::class)
-            ->add('sessions')
-            ->add('submit', SubmitType::class)
+            //->add('sessions') entitytype + paramètres
+            ->add('ajouter', SubmitType::class)
         ;
     }
 
